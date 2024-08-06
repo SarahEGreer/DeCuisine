817d3a28f60ed172b03d22fa8113886e2bb3953a
David Hall <theonetruedavidhall@gmail.com>
2024-08-06 13:42:56 -0500

    8/6/24 2:45estpm fixing url.'

--- java/src/main/java/com/techelevator/controller/RecipeController.java
+++ java/src/main/java/com/techelevator/controller/RecipeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/recipe")
+@RequestMapping("/recipes")
 public class RecipeController {
     private final RecipeDao recipeDao;
 
