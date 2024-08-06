8806577c6a6c5e88fc084d298972b9dfe9fbe9f9
David Hall <theonetruedavidhall@gmail.com>
2024-08-06 08:55:00 -0500

    added controller logic.

--- java/src/main/java/com/techelevator/controller/RecipeController.java
+++ java/src/main/java/com/techelevator/controller/RecipeController.java
@@ -1,8 +1,42 @@
 package com.techelevator.controller;
 
+import com.techelevator.dao.RecipeDao;
+import com.techelevator.model.Recipe;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
-
+@RequestMapping("/recipe")
 public class RecipeController {
+    private final RecipeDao recipeDao;
+
+    public RecipeController(RecipeDao recipeDao) {
+        this.recipeDao = recipeDao;
+    }
+
+    @GetMapping
+    public List<Recipe> getAllRecipes() {
+        List<Recipe> recipes = null;
+        try{
+            recipes = recipeDao.getAllRecipes();
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to connect to server or database", e);
+        }
+        return recipes;
+    }
+
+
+
+
+
+
+
+
 }
