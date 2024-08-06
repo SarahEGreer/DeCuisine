179d00d124f749563eb4ca72b8fe771a8916ad86
Karyssa Miller <karyssakmiller@gmail.com>
2024-08-06 12:10:36 -0400

    Updated user.sql to have all of the grant access permissions together

--- java/database/user_step2.sql
+++ java/database/user_step2.sql
@@ -24,7 +24,8 @@ GRANT USAGE, SELECT
 ON ALL SEQUENCES IN SCHEMA public
 TO final_capstone_appuser;
 
-
+-- GRANT ALL ON ALL TABLES IN SCHEMA public TO final_capstone_owner;
+-- GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO final_capstone_owner;
 -- GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO final_capstone_appuser;
 -- GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO final_capstone_appuser;
 
