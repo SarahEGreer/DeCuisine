289fce0ec561d4fc43ebb6a85773063809a42cb1
David Hall <theonetruedavidhall@gmail.com>
2024-08-08 12:50:18 -0500

    added logic for controller, needs to be tested via postman.

--- java/src/main/java/com/techelevator/controller/MealplanController.java
+++ java/src/main/java/com/techelevator/controller/MealplanController.java
@@ -2,22 +2,92 @@ package com.techelevator.controller;
 
 
 import com.techelevator.dao.MealplanDao;
+import com.techelevator.dao.UserDao;
+import com.techelevator.dao.UserDao;
+import com.techelevator.exception.DaoException;
+import com.techelevator.model.Mealplan;
+import com.techelevator.model.MealplanDto;
+import com.techelevator.model.User;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/mealplans")
+@RequestMapping("/mealplan")
 @PreAuthorize("isAuthenticated()")
 
 public class MealplanController {
     private final MealplanDao mealplanDao;
+    private final UserDao userDao;
 
-    public MealplanController(MealplanDao mealplanDao) {
+    public MealplanController(MealplanDao mealplanDao, UserDao userDao) {
         this.mealplanDao = mealplanDao;
+        this.userDao = userDao;
     }
+    @GetMapping
+    public List<Mealplan> getAllMealplans() {
+        List<Mealplan> mealplans = new ArrayList<>();
+        try {
+            mealplans = mealplanDao.getAllMealplans();
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to connect to server or database", e);
+        }
+        return mealplans;
+    }
+    @GetMapping( path = "/{mealplanId}")
+    public Mealplan getMealplanByMealplanId(@PathVariable int mealplanId) {
+        try{
+            return mealplanDao.getMealplanByMealplanId(mealplanId);
+        }catch (CannotGetJdbcConnectionException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to connect to server or database", e);
+        }
+    }
+
+    @GetMapping( path = "/details/{mealplanId}")
+    public MealplanDto getMealplanDetailsWithSchedule(@PathVariable int mealplanId) {
+        try{
+            return mealplanDao.getMealplanDetailsWithSchedule(mealplanId);
+        }catch (CannotGetJdbcConnectionException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to connect to server or database", e);
+        }
+    }
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void createMealplan(@RequestBody MealplanDto mealplanDto, Principal principal){
+        try{
+            String userName = principal.getName();
+            User currentUser = userDao.getUserByUsername(userName);
+            mealplanDao.createMealplan(mealplanDto, currentUser.getId());
+        }catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        } catch (DataIntegrityViolationException e) {
+            throw new DaoException("Data integrity violation", e);
+        }
+    }
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/{recipeId}")
+    public  void updateMealplan(@RequestBody MealplanDto mealplanDto, @PathVariable int mealplanId) {
+        try{
+            mealplanDao.updateMealplan(mealplanDto, mealplanId);
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to connect to server or database", e);
+        } catch (DataIntegrityViolationException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Data integrity violation", e);
+        }
+    }
+
+
+
+
+
 
 
 }
